f2c9e6191a538e33a72d97237bfa66d8d4dcb77f
GoobertonTheThird <byrne@etown.edu>
2025-08-04 15:18:00 -0400

    the move processor can now choose the right piece based on input

--- ChessBoard/moveProcessor.php
+++ ChessBoard/moveProcessor.php
@@ -1,57 +1,66 @@
 <?php
 $currentBoardState=[
-    ["wr","wn","wb","wq","wk","wb","wk","wr"],
-    ["wp","wp","wp","wp","wp","wp","wp","wp"],
+    ["wr","wn","wb","wq","wk","wb","wn","wr"],
+    ["wp","wp Correct","wp","wp","wp","wp","wp","wp"],
     ["0","0","0","0","0","0","0","0"],
     ["0","0","0","0","0","0","0","0"],
     ["0","0","0","0","0","0","0","0"],
     ["0","0","0","0","0","0","0","0"],
     ["bp","bp","bp","bp","bp","bp","bp","bp"],
-    ["br","bn","bb","bq","bk","bb","bk","br"]];
+    ["br","bn","bb","bq","bk","bb","bn","br"]];
 
-$playerMove = ["a2","a4"];
+$playerMove = ["b2","a4"];
 
-mover($playerMove);
+mover($currentBoardState,$playerMove);
 
-function mover($playersMove) {
-    $initialX = substr($playersMove[0],0,1);
-    $initialY = substr($playersMove[0],1,1);
+function mover($currentBoardState,$playersMove) {
+    $initialY = substr($playersMove[0],0,1);
+    $initialX = substr($playersMove[0],1,1);
 
-    $endX = substr($playersMove[1],0,1);
-    $endY = substr($playersMove[1],1,1);
+    $endY = substr($playersMove[1],0,1);
+    $endX = substr($playersMove[1],1,1);
 
-    $initialX = changeintonumber($initialX);
-    $endX = changeintonumber($endX);
+    $initialY = changeintonumber($initialY);
+    $endY = changeintonumber($endY);
 
-    print_r($initialX);
-    print("<br>");
+    $initialX -= 1;
+    $endX -= 1;
     print_r($initialY);
     print("<br>");
-    print_r($endX);
+    print_r($initialX);
     print("<br>");
     print_r($endY);
     print("<br>");
+    print_r($endX);
+    print("<br>");
+    print_r(whatPieceWeMoving($currentBoardState,$initialX,$initialY));
 
 }
 
 function changeintonumber($inputLetter){
     if ($inputLetter == "a"){
-        return "1";
+        return "0";
     } else if ($inputLetter == "b"){
-        return "2";
+        return "1";
     } else if ($inputLetter == "c"){
-        return "3";
+        return "2";
     } else if ($inputLetter == "d"){
-        return "4";
+        return "3";
     } else if ($inputLetter == "e"){
-        return "5";
+        return "4";
     } else if ($inputLetter == "f"){
-        return "6";
+        return "5";
     } else if ($inputLetter == "g"){
-        return "7";
+        return "6";
     } else if ($inputLetter == "h"){
-        return "8";
+        return "7";
     }
+}
+
+function whatPieceWeMoving($currentBoardState,$X,$Y) {
+    
+    $returning = $currentBoardState[$Y][$X];
+    return $returning;
 }
 
 ?>
