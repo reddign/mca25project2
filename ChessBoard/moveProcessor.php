6a9c4aee0ef08eb7904844a298398e98c8793dae
GoobertonTheThird <byrne@etown.edu>
2025-08-04 17:49:34 -0400

    the move processor now knows which team the piece is on.

--- ChessBoard/moveProcessor.php
+++ ChessBoard/moveProcessor.php
@@ -1,7 +1,7 @@
 <?php
 $currentBoardState=[
     ["wr","wn","wb","wq","wk","wb","wn","wr"],
-    ["wp","wp Correct","wp","wp","wp","wp","wp","wp"],
+    ["wp","wp","wp","wp","wp","wp","wp","wp"],
     ["0","0","0","0","0","0","0","0"],
     ["0","0","0","0","0","0","0","0"],
     ["0","0","0","0","0","0","0","0"],
@@ -9,7 +9,7 @@ $currentBoardState=[
     ["bp","bp","bp","bp","bp","bp","bp","bp"],
     ["br","bn","bb","bq","bk","bb","bn","br"]];
 
-$playerMove = ["b2","a4"];
+$playerMove = ["a2","a4"];
 
 mover($currentBoardState,$playerMove);
 
@@ -25,15 +25,22 @@ function mover($currentBoardState,$playersMove) {
 
     $initialX -= 1;
     $endX -= 1;
+
+    $pieceType = whatPieceWeMoving($currentBoardState,$initialX,$initialY);
+    $team = substr($pieceType,0,1);
+
+    print("<br> Beginning Y:");
     print_r($initialY);
-    print("<br>");
+    print("<br> Beginning X:");
     print_r($initialX);
-    print("<br>");
+    print("<br> ending Y:");
     print_r($endY);
-    print("<br>");
+    print("<br> ending X:");
     print_r($endX);
-    print("<br>");
-    print_r(whatPieceWeMoving($currentBoardState,$initialX,$initialY));
+    print("<br> Type:");
+    print_r($pieceType);
+    print("<br> Side:");
+    print_r($team);
 
 }
 
@@ -58,9 +65,27 @@ function changeintonumber($inputLetter){
 }
 
 function whatPieceWeMoving($currentBoardState,$X,$Y) {
-    
-    $returning = $currentBoardState[$Y][$X];
+    $returning = $currentBoardState[$X][$Y];
     return $returning;
 }
+
+function whitePawn($currentBoardState,$X,$Y) {
+
+}
+
+function blackPawn($currentBoardState,$X,$Y) {
+    
+}
+
+function knight($currentBoardState,$X,$Y) {
+
+}
+
+function bishop($currentBoardState,$X,$Y) {
+    
+}
+
+
+
 
 ?>
