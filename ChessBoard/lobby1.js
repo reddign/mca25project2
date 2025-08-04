6146dce0b2fe74f529aff856ee7d756d5e371897
PieSavage <Perkins@etown.edu>
2025-08-04 12:26:59 -0400

    Adding the chess board to the lobby, and adding the setup for adding images to the chess board

---
+++ ChessBoard/lobby1.js
@@ -0,0 +1,49 @@
+let canvas = document.querySelector("canvas")
+let graphics = canvas.getContext("2d")
+
+
+let currentBoardState=[
+    ["wr","wn","wb","wq","wk","wb","wk","wr"],
+    ["wp","wp","wp","wp","wp","wp","wp","wp"],
+    ["0","0","0","0","0","0","0","0"],
+    ["0","0","0","0","0","0","0","0"],
+    ["0","0","0","0","0","0","0","0"],
+    ["0","0","0","0","0","0","0","0"],
+    ["bp","bp","bp","bp","bp","bp","bp","bp"],
+    ["br","bn","bb","bq","bk","bb","bk","br"]
+];
+
+
+displayPieces()
+function displayPieces(){
+    //Draws the pieces to the canvas based on the current board state
+}
+
+selectPiece();
+function selectPiece(){
+    //on click, if there is a piece there, then select it. Press Esc to cancel selection
+
+
+}
+
+
+
+
+
+drawBoard()
+function drawBoard(){
+    //Draws the board to the canvas
+    let offset=0;
+    graphics.fillStyle="White"
+
+    for(j=0;j<480;j+=60){
+        if(offset==0){
+            offset=60
+        }else{
+            offset=0
+        }
+        for(i=0;i<480;i+=120){
+            graphics.fillRect(i+offset, j, 60, 60)
+        }
+    }
+}
--- ChessBoard/lobby1.php
+++ ChessBoard/lobby1.php
@@ -9,5 +9,9 @@
     <p2>
     <?php include "moveProcessor.php"; ?>
     </p2>
+    
+    <canvas style="border:2px solid #000000;background-color: black; display: block; margin-left: auto; margin-right: auto;" width = "480;" height = "480;"
+    ></canvas>
+    <script src = "lobby1.js"></script>
 </body>
 </html>
