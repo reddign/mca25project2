45a666b94dc365e8ec81f0dac0d5acea629564de
NekoZViV1D <lama@etown.edu>
2025-08-05 12:55:23 -0400

    corrected links for chessboards

--- includes/navbar.php
+++ includes/navbar.php
@@ -5,19 +5,19 @@
           <a href="index.php" class="w3-bar-item w3-button">Home</a>
           <!-- Right-sided navbar links. Hide them on small screens -->
           <div class="w3-right w3-hide-small">
-            <a href="lobby1.php" class="w3-bar-item w3-button">
+            <a href="../ChessBoard/lobby1.php" class="w3-bar-item w3-button">
                 <img id="logo" src="../chessImages/lobby1.png" alt="logo" height="40px">
             </a>
-            <a href="lobby2.php" class="w3-bar-item w3-button">
+            <a href="../ChessBoard/lobby2.php" class="w3-bar-item w3-button">
                 <img id="logo" src="../chessImages/lobby2.png" alt="logo" height="40px">
             </a>
-            <a href="lobby3.php" class="w3-bar-item w3-button">
+            <a href="../ChessBoard/lobby3.php" class="w3-bar-item w3-button">
                 <img id="logo" src="../chessImages/lobby3.png" alt="logo" height="40px">
             </a>
-            <a href="lobby4.php" class="w3-bar-item w3-button">
+            <a href="../ChessBoard/lobby4.php" class="w3-bar-item w3-button">
                 <img id="logo" src="../chessImages/lobby4.png" alt="logo" height="40px">
             </a>
-            <a href="lobby5.php" class="w3-bar-item w3-button">
+            <a href="../ChessBoard/lobby5.php" class="w3-bar-item w3-button">
                 <img id="logo" src="../chessImages/lobby5.png" alt="logo" height="40px">
             </a>
           </div>
