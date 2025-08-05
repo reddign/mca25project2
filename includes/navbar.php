cdb7333975e6d817f13877a21456493ebcfbddfa
NekoZViV1D <lama@etown.edu>
2025-08-05 12:28:46 -0400

    added navbar, links to other lobbies, changed main page text and leaderboard db updated

---
+++ includes/navbar.php
@@ -0,0 +1,24 @@
+<!-- This is were the users will navigate to join lobbys after signing in -->
+<div class="w3-top">
+        <div class="w3-bar w3-white w3-padding w3-card" style="letter-spacing:4px;">
+          <a href="index.php" class="w3-bar-item w3-button">Home</a>
+          <!-- Right-sided navbar links. Hide them on small screens -->
+          <div class="w3-right w3-hide-small">
+            <a href="lobby1.php" class="w3-bar-item w3-button">
+                <img id="logo" src="../testing/chessImages/lobby1.png" alt="logo" height="40px">
+            </a>
+            <a href="lobby2.php" class="w3-bar-item w3-button">
+                <img id="logo" src="../testing/chessImages/lobby2.png" alt="logo" height="40px">
+            </a>
+            <a href="lobby3.php" class="w3-bar-item w3-button">
+                <img id="logo" src="../testing/chessImages/lobby3.png" alt="logo" height="40px">
+            </a>
+            <a href="lobby4.php" class="w3-bar-item w3-button">
+                <img id="logo" src="../testing/chessImages/lobby4.png" alt="logo" height="40px">
+            </a>
+            <a href="lobby5.php" class="w3-bar-item w3-button">
+                <img id="logo" src="../testing/chessImages/lobby5.png" alt="logo" height="40px">
+            </a>
+          </div>
+        </div>
+      </div>
--- index.php
+++ index.php
@@ -1,31 +1,44 @@
 <html lang="en">
 <head>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>testing chess</title>
     <link href="chessstyle.css" rel="stylesheet">
+    <?php
+
+    include "includes/header.php";
+    include "includes/navbar.php";
+    
+    ?>
 </head>
 <body style="background-color: blanchedalmond;">
-    <h1>This is chess</h1>
-    <h2>I am john chess</h2>
+    <br><br><br>
+    <h1>Multiplayer Chess</h1>
+    <h2>Welcome to our chess website. Click on one of the five chess pieces above to join a lobby! (Max 2 players per lobby)</h2>
+
+
+<!-- add the 5 lobbies' buttons !-->
+
+
 <br><br><br><br><br><br><br><br><br><br><br><br>
     <table>
         <h1>Leaderboard</h1>
             <tr><th>Username</th><th>Wins</th></tr>
            
-            <?php
-
-require "scoreboarddb.php";                    
+            
+<?php
+require "dbCreds.php";                    
 $mysqli = new mysqli($servername,$username,$password,$database);
                //make a SQL message
-$sql = "select * FROM scores
-JOIN users on (users.id=scores.id);
+$sql = "select userid,score FROM scores
+JOIN users on (username=scores.userid);";
 
-//send SQL and get result
 $result = $mysqli -> query($sql);
 $rows = $result -> fetch_all(MYSQLI_ASSOC);
 
-//add loop
+
 foreach($rows as $row){
     print("<tr><td>{$row['username']}</td><td>{$row['score']}</td></tr>");            
 }
+?>
