b67f36811ed7698c319f625aa779cd96466a2260
Wesley Groff <groff1@etown.edu>
2025-08-05 13:53:26 -0400

    Amended sql queries, but still requires further revision

--- ChessBoard/updateBoard.php
+++ ChessBoard/updateBoard.php
@@ -1,19 +1,19 @@
 <?php
 require "../dbCreds.php";
 
-$chessGame = //Pull current chess game lobby from somewhere?
-$piecePosition = //Get the position of the current piece
-$requestPosition = //Get the position of the requested move (both the above will be entered as like a2)
+$chessGame = "chessGame1";//Pull current chess game lobby from somewhere?
+$piecePosition = "b1";//Get the position of the current piece
+$requestPosition = "a2";//Get the position of the requested move (both the above will be entered as like a2)
 
 
 $mysqli = new mysqli($servername,$username,$password,$database);
 
 //set taken piece to attacking piece
-$sql = "update '{$chessGame}' set '{substr($requestPosition,0,1)}'=(select '{substr($piecePosition,0,1)}' where id= '{substr($piecePosition,1,1)}') where id='{substr($requestPosition,1,1)}';"
+$sql = "update '{$chessGame}' set SUBSTRING('{$requestPosition}',1,1)=(select SUBSTRING('{$piecePosition}',1,1)) where id= SUBSTRING('{$piecePosition}',2,1)) where id=SUBSTRING('{$requestPosition}',2,1);";
 $result=$mysqli -> query($sql);
 
 //set attacking piece's previous position to 0
-$sql = "update '{$chessGame}' set '{substr($piecePosition,0,1)}'=0 where id='{substr($piecePosition,1,1)}';"
+$sql = "update '{$chessGame}' set SUBSTRING('{$piecePosition}',1,1)=0 where id=SUBSTRING('{$piecePosition}',1,1);";
 $result=$mysqli -> query($sql);
 
 //TODO: redirect user back to chess board
--- dbCreds.php
+++ dbCreds.php
@@ -1,10 +1,10 @@
 <?PHP
 //connect to the db
 
-    $servername = "???";
-    $username = "???";
-    $password = "???";
-    $database = "???";
+    $servername = "195.35.59.14";
+    $username = "u121755072_groff1";
+    $password = "435Imn4}";
+    $database = "u121755072_groff1db";
 
     //Use line 
     // require "../dbcreds.php"; 
