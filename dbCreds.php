ec067a4ead6df103eeb87ae6249df7523575d868
NekoZViV1D <lama@etown.edu>
2025-08-05 12:39:54 -0400

    j

--- dbCreds.php
+++ dbCreds.php
@@ -1,10 +1,10 @@
 <?PHP
 //connect to the db
 
-    $servername = "195.35.59.14";
-    $username = "u121755072_chess";
-    $password = "Mu4[XM@[D6&d";
-    $database = "u121755072_chessdb";
+    $servername = "???";
+    $username = "???";
+    $password = "???";
+    $database = "???";
 
     //Use line 
     // require "../dbcreds.php"; 
