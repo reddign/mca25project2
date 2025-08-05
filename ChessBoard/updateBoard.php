dbba1f9e174eb4febca35a5b27164345aca2d88e
Wesley Groff <groff1@etown.edu>
2025-08-05 11:16:22 -0400

    tested and fixed errors in sql statements

--- ChessBoard/updateBoard.php
+++ ChessBoard/updateBoard.php
@@ -9,11 +9,11 @@ $requestPosition = //Get the position of the requested move (both the above will
 $mysqli = new mysqli($servername,$username,$password,$database);
 
 //set taken piece to attacking piece
-$sql = "update table '{$chessGame}' set '{substr($requestPosition,0,1)}'=(select '{substr($piecePosition,0,1)}' where id= '{substr($piecePosition,1,1)}') where id='{substr($requestPosition,1,1)}'"
+$sql = "update '{$chessGame}' set '{substr($requestPosition,0,1)}'=(select '{substr($piecePosition,0,1)}' where id= '{substr($piecePosition,1,1)}') where id='{substr($requestPosition,1,1)}';"
 $result=$mysqli -> query($sql);
 
 //set attacking piece's previous position to 0
-$sql = "update table '{$chessGame}' set '{substr($piecePosition,0,1)}'=0 where id='{substr($piecePosition,1,1)}'"
+$sql = "update '{$chessGame}' set '{substr($piecePosition,0,1)}'=0 where id='{substr($piecePosition,1,1)}';"
 $result=$mysqli -> query($sql);
 
 //TODO: redirect user back to chess board
