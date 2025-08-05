4437c78466be68e19927c8b78f305ae9527bd399
PieSavage <Perkins@etown.edu>
2025-08-05 11:26:27 -0400

    Added displaying all the chess images based on the current position, and added being able to select pieces

--- ChessBoard/lobby1.php
+++ ChessBoard/lobby1.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Super Cool Chess</title>
     
 </head>
 <body>
@@ -10,8 +10,27 @@
     <?php include "moveProcessor.php"; ?>
     </p2>
     
-    <canvas style="border:2px solid #000000;background-color: black; display: block; margin-left: auto; margin-right: auto;" width = "480;" height = "480;"
+    <canvas style="border:2px solid #000000;background-color: grey; display: block; margin-left: auto; margin-right: auto;" width = "480;" height = "480;"
+    onmousedown="selectPiece(event);"
     ></canvas>
+
+    <br>
+    <button onclick="displayPieces(event)">Start Game</button>
+    <img style="height: 0px" src="chessImages/wPawn.png" id="whitePawn">
+    <img style="height: 0px" src="chessImages/bPawn.png" id="blackPawn">
+    <img style="height: 0px" src="chessImages/wBishop.png" id="whiteBishop">
+    <img style="height: 0px" src="chessImages/bBishop.png" id="blackBishop">
+    <img style="height: 0px" src="chessImages/wKnight.png" id="whiteKnight">
+    <img style="height: 0px" src="chessImages/bKnight.png" id="blackKnight">
+    <img style="height: 0px" src="chessImages/wRook.png" id="whiteRook">
+    <img style="height: 0px" src="chessImages/bRook.png" id="blackRook">
+    <img style="height: 0px" src="chessImages/wQueen.png" id="whiteQueen">
+    <img style="height: 0px" src="chessImages/bQueen.png" id="blackQueen">
+    <img style="height: 0px" src="chessImages/wKing.png" id="whiteKing">
+    <img style="height: 0px" src="chessImages/bKing.png" id="blackKing">
+
+
     <script src = "lobby1.js"></script>
+
 </body>
 </html>
