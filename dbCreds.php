8ae294785a7a53f8830e4814e8ad69c1a0734d88
Wesley Groff <groff1@etown.edu>
2025-08-06 10:37:51 -0400

    Changed over to new database

--- dbCreds.php
+++ dbCreds.php
@@ -2,9 +2,9 @@
 //connect to the db
 
     $servername = "195.35.59.14";
-    $username = "u121755072_groff1";
-    $password = "435Imn4}";
-    $database = "u121755072_groff1db";
+    $username = "u121755072_chess";
+    $password = "Mu4[XM@[D6&d";
+    $database = "u121755072_chessdb";
 
     //Use line 
     // require "../dbcreds.php"; 
