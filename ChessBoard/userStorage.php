5d0ec3af9166860674bb17f0ec3116de0783e987
GoobertonTheThird <byrne@etown.edu>
2025-08-06 12:34:19 -0400

    adding user storage php file things

---
+++ ChessBoard/userStorage.php
@@ -0,0 +1,23 @@
+<?php
+//check if id 2 has anything, if it's null then put the username there.
+//check if id 3 has anything, if it's null then put the username there. 
+//if both have something then kick the user back to the lobbies page.
+
+//username - store in the table uder turn id 2 if white or turn id 3 if black
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
