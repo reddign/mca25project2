0f306c8186d51b5f1fd91524378d9d3dad63e57d
Wesley Groff <groff1@etown.edu>
2025-08-07 11:54:27 -0400

    finished track players script

--- ChessBoard/trackPlayers.php
+++ ChessBoard/trackPlayers.php
@@ -1,15 +1,34 @@
 <?php
+session_start();
 
 require "../dbCreds.php";
 
 $mysqli = new mysqli($servername,$username,$password,$database);
 
-$sql = "select turn from $_SESSION['game']";
+$sql = "select turn from {$_SESSION['game']}";
 
 //Send SQL and get results
 $result = $mysqli -> query($sql);
 $sqlresult = $result -> fetch_all(MYSQLI_ASSOC);
+$complete = false
 
-print_r($sqlresult);
+switch($sqlresult[1]){
+    case null:
+        $sql = "update {$_SESSION['game']} set turn = {$_SESSION['username']} where id = 2;";
+        break;
+    case $_SESSION['username']:
+        break;
+    default:
+    switch($sqlresult[2]){
+        case null:
+        $sql = "update {$_SESSION['game']} set turn = {$_SESSION['username']} where id = 3;";
+        break;
+    case $_SESSION['username']:
+        break;
+    default:
+    header("location:../index.php");
+    }
+    }
+    $result = $mysqli -> query($sql);
 
 ?>
