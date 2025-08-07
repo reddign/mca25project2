017359f73b9eebbe7ef43968935be8873c2ae02b
NekoZViV1D <lama@etown.edu>
2025-08-07 09:52:45 -0400

    hopefully finally fixed favicon

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chess</title>
-    <link rel="stylesheet" href="includes/navstyle.css">
-    <link rel="icon" type="image/x-icon" href="/webicon_2.png">
+    <link rel="stylesheet" href="navstyle.css">
+    <link rel="icon" type="image/x-icon" href="../chessImages/webicon_2.png">
 </head>
